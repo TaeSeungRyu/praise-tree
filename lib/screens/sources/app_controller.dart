80dc41d3b9c84d583b17a99417fa07fa693ed36d
ts.Ryu <lts06069@naver.com>
2024-12-18 09:12:14 +0900

    칭찬 나무 프로젝트
    파일 선택 후 중앙 자르기 기능 구현 진행 중
     - stack 위젯에서 이미지 선택 후 캔버스에 가운데 영역 드로잉 > 기초 완료(리팩토링 전)
     - 사용자의 버튼 액션 후 해당 이미지 영역만큼 이미지 잘라내기 > 대기
     - 전체 리팩토링 > 대기

--- lib/screens/sources/app_controller.dart
+++ lib/screens/sources/app_controller.dart
@@ -1,8 +1,45 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 
 class AppController extends GetxController with WidgetsBindingObserver {
   static AppController get to => Get.find();
 
   RxString testText = 'asdf1234'.obs;
+  Rx<File> previewImage = File("").obs;
+
+  final GlobalKey cropKey = GlobalKey();
+  final double overlaySize = 200; // 구멍 크기
+  final Rx<Offset> center = const Offset(200, 300).obs; // 구멍의 위치
+
+  Future<void> cropImage() async {
+    var isFileExists =await previewImage.value.exists();
+    if (!isFileExists) return;
+
+    RenderRepaintBoundary boundary =
+    cropKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData =
+    await image.toByteData(format: ui.ImageByteFormat.png);
+
+    if (byteData != null) {
+      final pngBytes = byteData.buffer.asUint8List();
+      // 저장하거나 다른 작업에 활용 가능
+      // File croppedImage = File('path_to_save').writeAsBytesSync(pngBytes);
+      debugPrint("이미지 잘라내기 성공!");
+    }
+  }
+
+
+  void initCenterPosition(context) {
+    final screenSize = MediaQuery.of(context).size;
+    center.value = Offset(screenSize.width / 2, screenSize.height / 2);
+    debugPrint("center: ${center.value}");
+  }
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,62 +1,119 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:my_heart_son/screens/sources/app_controller.dart';
-import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Splash extends GetView<AppController> {
   const Splash({super.key});
 
   static const routeName = "/splash";
 
-
-  initCropped(BuildContext context, String sourcePath) async {
-
-    CroppedFile? croppedFile =  await ImageCropper().cropImage(
-      sourcePath: sourcePath,
-      aspectRatioPresets: [
-        CropAspectRatioPreset.square,
-        CropAspectRatioPreset.ratio3x2,
-        CropAspectRatioPreset.original,
-        CropAspectRatioPreset.ratio4x3,
-        CropAspectRatioPreset.ratio16x9
-      ],
-      uiSettings: [
-        AndroidUiSettings(
-            toolbarTitle: 'Cropper',
-            toolbarColor: Colors.deepOrange,
-            toolbarWidgetColor: Colors.white,
-            initAspectRatio: CropAspectRatioPreset.original,
-            lockAspectRatio: false),
-      ],
-    );
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      debugPrint("image path: ${image.path}");
+      controller.previewImage.value = File(image.path);
+      controller.previewImage.refresh();
+      debugPrint("image path: ${controller.previewImage.value}");
+    }
   }
 
+  Widget generateCropToImageWidget() {
+    return Expanded(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          InteractiveViewer(
+            clipBehavior: Clip.none,
+            minScale: 0.5,
+            maxScale: 2.0,
+            child: RepaintBoundary(
+              key: controller.cropKey,
+              child: Image.file(
+                controller.previewImage.value!,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // 구멍이 있는 오버레이
+          CustomPaint(
+            size: Size.zero,
+            painter: HolePainter(controller.center.value, controller.overlaySize),
+            child: const IgnorePointer(
+              ignoring: true, // 터치 이벤트를 무시
+            ),
+          ),
+
+        ],
+      ),
+    );
+    //TODO : 이미지 잘라서 뷰 하는거!!
+    // ElevatedButton(
+    // onPressed: controller.cropImage(),
+    // child: Text("Crop Image"),
+    // );
+  }
+
+  //TODO : 이미지 가운데 구멍뚫기 기능 만들고 분리하기 진행중
   @override
   Widget build(BuildContext context) {
-
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      initCropped(context, "assets/success.png");
+      controller.initCenterPosition(context);
     });
 
-    return
-      SafeArea(child: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(controller.testText.value),
-            Image.asset(
-              "assets/success.png",
-              fit: BoxFit.scaleDown,
-              width: 40,
-              height: 40,
-            )
-          ],
-        ),
-      ),
-    ),);
-
+    return Obx(() => SafeArea(
+          child: Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  controller.previewImage.value!.existsSync()
+                      ? generateCropToImageWidget()
+                      : ElevatedButton(
+                          onPressed: _pickImage,
+                          child: const Text("Pick an Image"),
+                        )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
+}
 
+// 구멍 오버레이를 그리는 Painter
+class HolePainter extends CustomPainter {
+  final Offset center;
+  final double size;
+
+  HolePainter(this.center, this.size);
+
+  @override
+  void paint(Canvas canvas, Size screenSize) {
+
+
+    //IgnorePointer 가 있는 경우에 offset 값을 0 으로 해야 중앙으로 옵니다.
+    //TODO : 정리 필요!
+    debugPrint("Center Offset: $center, Screen Size: $screenSize"); // 디버그 출력
+
+    Paint paint = Paint()
+      ..color = Colors.black.withOpacity(0.2)
+      ..style = PaintingStyle.fill;
+
+    // 전체 화면을 덮는 반투명 레이어
+    canvas.drawRect(Offset.zero & screenSize, paint);
+    // 구멍 부분
+    Paint circleColor = Paint()
+      ..color = Colors.blue.withOpacity(0.3)
+      ..style = PaintingStyle.fill;
+    paint.blendMode = BlendMode.clear;
+    canvas.drawCircle(Offset(0, 0), size / 2, circleColor);
+  }
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
 }
